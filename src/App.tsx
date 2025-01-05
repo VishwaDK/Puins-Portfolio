b8c00df785111bcecfec99e36dc01765850c8540
Vinura de Silva <m.v.r.desilva@gmail.com>
2025-01-05 12:06:38 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center">
+        I am Vishwa Bulegoda
+      </div>
+    </>
+  );
+}
+
+export default App;
